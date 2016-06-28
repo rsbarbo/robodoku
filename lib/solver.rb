f43a7fad0da7fb97a271cccbd8696b8404b7a2e0
Raphael Barbo <rsbarbo@gmail.com>
2016-06-27 19:12:08 -0600

    initial commit

--- lib/solver.rb
+++ lib/solver.rb
@@ -1,3 +1,31 @@
+require "pry"
+
 class Solver
-  # your excellent code here
+
+  # we need to count the number of rows and columns
+
+  def character_counter(sudoku)
+    sudoku.size
+  end
+
+  def missing_characters_in_row(sudoku)
+    array = []
+    highest_number = character_counter(sudoku)
+    highest_number.times do
+      if !sudoku.chars.include?(highest_number.to_s)
+        array.push(highest_number)
+      end
+      highest_number -= 1
+    end
+    array
+  end
+
+  def missing_characters_in_column
+
+  end
+
+
+
+  #compare up and down for repeated
+
 end
--- test/solver_test.rb
+++ test/solver_test.rb
@@ -1,8 +1,28 @@
 require "minitest"
 require "minitest/autorun"
+require "./lib/solver"
 
 class SolverTest < Minitest::Test
-  def test_it_solves
-    assert false, "make it solve!"
+
+  def test_read_file_per_line
+    
   end
+
+  def test_counts_characters
+    assert_equal 2, Solver.new.character_counter("12")
+  end
+
+  def test_missing_characters_in_row
+    assert_equal [1], Solver.new.missing_characters_in_row(" 2")
+  end
+
+  def test_missing_characters_in_rows
+    assert_equal [2], Solver.new.missing_characters_in_row("1 ")
+  end
+
+  def test_missing_characters_in_column
+    assert_equal [1], Solver.new.missing_characters_in_column(" 2", "21")
+  end
+
+
 end
